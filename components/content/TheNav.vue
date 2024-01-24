2d3dbe4a935c3a085483b7fba00f5f378c582d5a
Veni Muthu <venimuthu@Installer-s-MacBook-Pro.local>
2024-01-24 14:17:43 -0600

    adding new details

--- components/content/TheNav.vue
+++ components/content/TheNav.vue
@@ -8,7 +8,7 @@
     <template v-slot:image>
       <v-img
         gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
-      ></v-img>
+      ><v-app-bar-nav-icon></v-app-bar-nav-icon></v-img>
     </template>
 
 
