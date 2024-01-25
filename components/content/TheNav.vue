ba3090bedc4e3d835836d692517c74ba81af05aa
Veni Muthu <venimuthu@Installer-s-MacBook-Pro.local>
2024-01-25 17:36:44 -0600

    adding sidebar

--- components/content/TheNav.vue
+++ components/content/TheNav.vue
@@ -6,9 +6,9 @@
       color="white"
       class="pl-5 pr-5"
     >
-      <div
+    <div
         v-if="!nav"
-        class="hover hamburger text-center hidden-sm-and-down"
+        class="hover hamburger text-center hidden-md-and-up"
         @click="toggleNav"
       >
         <v-icon icon="mdi-menu" size="large"></v-icon>
@@ -16,13 +16,12 @@
       </div>
       <div
         v-else
-        class="hover hamburger text-center hidden-sm-and-down"
+        class="hover hamburger text-center hidden-md-and-up"
         @click="toggleNav"
       >
         <v-icon icon="mdi-window-close" size="large"></v-icon>
         <div style="font-size: 10px; font-weight: 900">CLOSE</div>
       </div>
-      <v-spacer class="hidden-sm-and-down"></v-spacer>
       <img
         alt="i2i Logo"
         class="hover ml-4 mr-4"
@@ -34,31 +33,35 @@
       <div class="hover hidden-sm-and-down">
         <span
           style="
-            font-weight: 900 !important;
+          font-weight: 900 !important;
             font-size: 22px;
             letter-spacing: 0.004em;
           "
           class="agency app-title"
-          @click="routeToHome()"
-          >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ICJIA
-         
+          @click="$router.push('/')"
+          ><span></span>&nbsp;&nbsp;ICJIA
         </span>
       </div>
-
-      <v-spacer></v-spacer>
-      <span v-if="isMounted">
+      <div class="hover hidden-md-and-up hidden-xs">
         <span
-          v-for="(menu, index) in navMenu"
-          :key="`main-${index}`"
-          style="display: inline-block"
-        >
-          <span v-if="menu && menu?.children">
+          style="
+          font-weight: 900 !important;
+            font-size: 22px;
+            letter-spacing: 0.004em;
+          "
+          class="agency app-title"
+          @click="$router.push('/')"
+          ><span></span>&nbsp;&nbsp;ICJIA
+        </span>
+      </div>
+     
+      <span v-if="menu && menu?.children">
             <v-menu>
               <template #activator="{ props }">
                 <v-btn
                   variant="text"
                   size="default"
-                  class="hidden-sm-and-down"
+                  class="hidden-sm-and-down navItem"
                   v-bind="props"
                 >
                   {{ menu.main }}
@@ -79,7 +82,7 @@
                   >
                   <v-list-item
                     v-if="child?.link"
-                    class=" hover"
+                    class="appNav hover"
                     :to="child?.external ? null : child?.link"
                     :href="child?.external ? child?.link : null"
                     :target="child?.external ? '_blank' : null"
@@ -94,59 +97,16 @@
                     >
                   </v-list-item>
                 </span>
+                
               </v-list>
             </v-menu>
-          </span>
-          <span v-else class="d-flex">
-            <v-btn
-              :to="menu?.external ? null : menu?.link"
-              :href="menu?.external ? menu.link : null"
-              :target="menu?.external ? '_blank' : null"
-              :aria-label="menu.main"
-              variant="text"
-              size="default"
-              class="hidden-sm-and-down navItem"
-              style="font-weight: 900 !important; font-size: 16px"
-              >{{ menu.main }}&nbsp;
-              <v-icon v-if="menu.icon" right small color="black">{{
-                menu.icon
-              }}</v-icon>
-            </v-btn>
-          </span>
-        </span>
-        
-        <span class="">
-          <v-tooltip activator="parent" location="bottom"> More </v-tooltip>
-          <v-menu>
-            <template #activator="{ props }">
-              <v-btn v-bind="props">
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
-            </template>
-
-            <v-list>
-              <v-list-item exact to="/">
-                <v-list-item-title style="font-size: 16px; font-weight: 700"
-                  ><v-icon size="x-small" icon="mdi-email" left></v-icon
-                  >&nbsp;&nbsp;&nbsp;&nbsp;HOME</v-list-item-title
-                >
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </span>
-       
-      </span>
-
-      <v-container>
-    <v-row>
-        <v-col>
-            <v-btn append-icon="$vuetify" to="/">Home</v-btn> 
-        </v-col>
-       
+          </span> 
+      
+      <v-row justify="end">
+      <v-btn append-icon="$vuetify" to="/">Home</v-btn> 
     </v-row>
-  </v-container>
-    </v-app-bar>
-  </div>
+    </v-app-bar>   
+    </div>
 </template>
 
 <script setup>
