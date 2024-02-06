bbb4be8df6f223645578f47e72b75cd8de6f11c5
Veni Muthu <venimuthu@Installer-s-MacBook-Pro.local>
2024-02-05 18:45:01 -0600

    adding details in content page

--- layouts/content.vue
+++ layouts/content.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
-
+        <br />
+        <br />
+        <br />
+        <v-spacer></v-spacer>
+        <v-form class="pl-2 mt-4" style="margin-top: -15px">
+              <v-text-field
+                id="textfield"
+                ref="textfield"
+                v-model="query"
+                autocomplete="off"
+                clearable
+                label="Search"
+              
+                style="font-weight: 900"
+                @input="instantSearch"
+                @keypress.enter.prevent
+              />
+            </v-form>
     </div>
 </template>
 
