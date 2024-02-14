364b9c9be3973755f8e6bd6b8adb9895769915c5
Veni Muthu <venimuthu@Installer-s-MacBook-Pro.local>
2024-02-14 10:19:22 -0600

    adding counter button in counter page

---
+++ pages/counter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button @click="store.increment">Click Me!</button>
+        <p> You clicked {{ store.count }} times.</p>
+    </div>
+</template>
+
+<script setup>
+import { useCounterStore } from "@/stores/searchStore"; 
+const store = useCounterStore();
+</script>
+
+<style scoped>
+
+
+</style>
--- stores/searchStore.js
+++ stores/searchStore.js
@@ -1,15 +1,18 @@
-import {defineStore} from 'pinia';
+import { defineStore, acceptHMRUpdate } from 'pinia';
 import {ref} from 'vue';
 
 export const useCounterStore=defineStore("counter",{
-   state:() =>{
-    return{
-    count: 0
-    };
-   },
+   state:() =>({
+    
+    count: 0,
+   
+   }),
     actions: {
         increment(){
             this.count++;
         },
     },
-})
+});
+if (import.meta.hot) {
+    import.meta.hot.accept(acceptHMRUpdate(useSearchStore, import.meta.hot));
+  }
