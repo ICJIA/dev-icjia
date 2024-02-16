11e8155c82db844b7113f223e6528d09ff66c1df
Veni Muthu <venimuthu@Installer-s-MacBook-Pro.local>
2024-02-16 14:06:03 -0600

    adding vuetify details

--- pages/counter.vue
+++ pages/counter.vue
@@ -3,7 +3,9 @@
   <v-container>
     <v-row align="center" justify="center">
         <v-col cols="12"><h1 align="center">Counter Page</h1></v-col>
+      
         <v-col cols="12">
+           
             <v-card class="" style="margin-top: -25px">
                 <v-img
                 cover
@@ -13,9 +15,15 @@
                 class="bg-grey-lighten-2"
                 height="300"
                 >
-                <div class="text-center px-12" style="margin-top: 100px">
-                <h1 class="nofo-title mt-3" style="color:rgb(11, 11, 11)">You have Clicked {{ store.count }} times!</h1>
-                <v-btn
+                <div class="text-center px-12" style="margin-top: 90px">
+                <h1 class="nofo-title mt-3" style="color:rgb(171, 190, 188)">You have Clicked {{ store.count }} times!</h1>
+                <v-divider
+        :thickness="5"
+        class="border-opacity-25"
+        color="color:rgb(18, 113, 102)"
+        ></v-divider>
+        <br />
+               <v-btn
                     variant="flat"
                     color="cyan-darken-4"
                     class="white--text mb-4"
@@ -25,16 +33,11 @@
                     :style="{ 'background-color': hover ? 'red' : '#BB86FC' }"
                 >Click Me!</v-btn>
                 </div>
-                <template v-slot:placeholder>
-          <v-row class="fill-height ma-0" align="center" justify="center">
-            <v-progress-circular
-              indeterminate
-              color="grey-lighten-5"
-            ></v-progress-circular>
-          </v-row>
-        </template>
+                
                 </v-img>
+                
             </v-card>
+       
             </v-col>   
         </v-row>
   </v-container>
