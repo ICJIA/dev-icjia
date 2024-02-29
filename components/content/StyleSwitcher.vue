66b802d630585236b983cacec1cb4b0ba3f9c10d
Veni Muthu <venimuthu@Installer-s-MacBook-Pro.local>
2024-02-29 09:54:45 -0600

    adding mdi menu to home page

--- components/content/StyleSwitcher.vue
+++ components/content/StyleSwitcher.vue
@@ -1,31 +1,41 @@
 <template>
     <div>
-        <v-switch
-                v-model="isMounted"
-              label="Dark"
-              color="red"
-              value="red"
-              hide-details
-              @click="toggleTheme"
-            ></v-switch>
-            <v-btn icon @click="toggleTheme">
-    <v-icon color="primary">mdi-theme-light-dark</v-icon>
-</v-btn>
+        
+            <v-btn 
+	v-if="isMounted"
+	icon="mdi-theme-light-dark"
+	size="large"
+	color="primary"
+	elevation="8"
+	@click.stop.prevent="toggleTheme()"
+	style="font-size: 30px;
+          margin-bottom: 30px;
+          margin-right: 30px;
+          z-index: 9999;
+        "
+	      position="fixed"
+        location="bottom left"
+        class="hidden-sm-and-down"
+      >
+      </v-btn>
+                                        
     </div>
 </template>
 ß
 <script setup>
 import { useTheme } from 'vuetify'
 import { useThemeStore } from '@/stores/searchStore'
-const isMounted =ref(false);
+let isMounted = ref(false);
 const theme = useTheme();
 
 const toggleTheme = () => {
     
   theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-  
-}
 
+}
+onMounted(() => {
+  isMounted.value = true;
+});
   
 
 </script>
