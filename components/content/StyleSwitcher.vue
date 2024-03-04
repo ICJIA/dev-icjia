603809c01d1f43e3089b9468b6deb892c388a93b
Veni Muthu <venimuthu@Installer-s-MacBook-Pro.local>
2024-03-04 08:52:35 -0600

    adding theme

--- components/content/StyleSwitcher.vue
+++ components/content/StyleSwitcher.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-
+    <v-btn 
+	
+	icon="mdi-theme-light-dark"
+	size="large"
+	color="primary"
+	elevation="8"
+@click="toggleTheme()"
+	style="font-size: 30px;
+          margin-bottom: 30px;
+          margin-right: 30px;
+          z-index: 9999;
+        "
+	      position="fixed"
+        location="bottom left"
+       
+      >
+      </v-btn>
   </div>
 </template>
 
 <script setup>
-
+const isMounted = ref(false);
+const themeStore = useThemeStore();
 </script>
 
 <style scoped>
