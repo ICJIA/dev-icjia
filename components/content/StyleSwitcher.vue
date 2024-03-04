6f038fd6e262c92d2d3f98fb85eee5337a64ba49
Veni Muthu <venimuthu@Installer-s-MacBook-Pro.local>
2024-03-04 08:40:03 -0600

    removing swich details

--- components/content/StyleSwitcher.vue
+++ components/content/StyleSwitcher.vue
@@ -1,54 +1,10 @@
 <template>
-    <div>
-        
-            <v-btn 
-	v-if="isMounted"
-	icon="mdi-theme-light-dark"
-	size="large"
-	color="primary"
-	elevation="8"
-	@click.stop.prevent="toggleTheme()"
-	style="font-size: 30px;
-          margin-bottom: 30px;
-          margin-right: 30px;
-          z-index: 9999;
-        "
-	      position="fixed"
-        location="bottom left"
-       
-      >
-      </v-btn>
-                                        
-    </div>
+  <div>
+
+  </div>
 </template>
-ß
+
 <script setup>
-import { useTheme } from 'vuetify'
-import { useThemeStore } from '@/stores/searchStore'
-const isMounted = ref(false);
-const themeStore = useThemeStore();
-const theme = useTheme();
-
-const toggleTheme = () => {
-   themeStore.toggleTheme(); 
-   theme.global.name.value=themeStore.isDarkTheme ? 'dark' : 'light';
- // theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-
-}
-onMounted(() => {
-  isMounted.value = true;
-
-  theme.global.name.value=themeStore.isDarkTheme ? 'dark' : 'light';
- // setThemeBasedOnUserPreference();
-});
-function setThemeBasedOnUserPreference(){
-  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-  theme.global.neme.value = prefersDarkScheme.matches ? 'dark': 'light';
-
-  prefersDarkScheme.addEventListener((e) => {
-    theme.global.name.value = e.matches ? 'dark':'light';
-  });
-}  
 
 </script>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
     },
     //...
   ],
-  ssr:true,
+  ssr:false,
 
 
   vite: {
