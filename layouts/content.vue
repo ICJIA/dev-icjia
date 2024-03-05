334554b558dea73184aa857eb555232334305615
Veni Muthu <venimuthu@Installer-s-MacBook-Pro.local>
2024-03-04 20:37:17 -0600

    private false

--- layouts/content.vue
+++ layouts/content.vue
@@ -5,25 +5,7 @@
 </template>
 
 <script setup>
-import Fuse from "fuse.js";
-import searchIndex from "~/src/searchIndex.json";
-const searchQuery = ref("");
-const options={
-    keys:["title","summary"],
-}
-const fuse=new Fuse(searchIndex, options);
-const searchResults=ref(fuse.search(searchQuery.value));
-const handleSearch=()=>{
-    searchResults.value=fuse.search(searchQuery.value);
-};
 
-const clearAll = () => {
-  searchQuery.value = "";
-  result.value = [];
-  showIndex.value = false;
-  const el = document.getElementById("textfield");
-  el.focus();
-};
 </script>
 
 <style lang="scss" scoped>
