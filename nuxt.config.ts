c86d4e4618550c73e28ca82d68a52b149bc5c4a9
Veni Muthu <venimuthu@Installer-s-MacBook-Pro.local>
2024-03-10 18:15:21 -0500

    adding limit data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
     }
   },
   'graphql-client': {
-    codegen: false,
+   
     tokenStorage: {
       mode: 'localStorage'
     }
--- pages/news.vue
+++ pages/news.vue
@@ -9,31 +9,56 @@
         </v-col>
       </v-row>
     </v-container>
-    <v-container fluid>
+   
+    <!--v-container fluid>
       <v-row>
-       
+       <v-col cols="12" md="4" v-for="post in data" :key="post.id">
+        <v-card>
+          
+          <v-card-title :title="post.title">
+            {{ post.title }}
+          </v-card-title>
+        </v-card>
+       </v-col>
       </v-row>
-
-    </v-container>
+      
+    </!--v-container-->
+    
       <div v-if="data">
         {{ data }}
+        <v-btn
+        class="ma-2"
+      
+        color="secondary"
+      @click="loadMore"
+     
+      >
+        LOAD MORE
+      </v-btn>
       </div>
+     
     </div>
+   
 </template>
 
 <script lang="ts" setup>
 definePageMeta({
   layout: 'content'
 });
-const { data } = await useAsyncGql({
-  operation: 'posts',
-  variables: { limit: 10 },
+const { data } = await useAsyncGql(
+'posts'
  
-});
+);
+const posts =computed(() => data.value)
+let page = 1;
 useHead({
   title:'NEWS',
   meta:[{ hid: 'description, content: description'}]
 })
+const loadMore = () => {
+  
+  
+};
 
 </script>
 
